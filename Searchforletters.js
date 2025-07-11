c57b1f41c28afa023f400e744a6b694fc9918f97
Emelyne749 <emelyneishimwe749@gmail.com>
2025-07-11 12:34:42 +0200

    adding a new kata

--- Searchforletters.js
+++ Searchforletters.js
@@ -1,12 +1,14 @@
+var str ="aaaaaaaa"
+var res = change(str);
 function change(string) {
   let output = Array(26).fill('0');
   string = string.toLowerCase();
   for (let i = 0; i < string.length; i++) {
     let char = string[i];
     if (char >= 'a' && char <= 'z') {
-      let index = char.charCodeAt(0) - 'a'.charCodeAt(0);
-      output[index] = '1';
-    }
+      output[char.charCodeAt(0) - 'a'.charCodeAt(0)] = '1';
+ }
   }
   return output.join('');
 }
+console.log(res)
