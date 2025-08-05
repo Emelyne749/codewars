68a44af17d4bd5b542f32bb2a3c185142ffc5bd6
Emelyne1024 <emelyneishimwe749@gmail.com>
2025-08-05 14:32:01 +0200

    Added another kata

---
+++ happyBirthDayDarling.js
@@ -0,0 +1,11 @@
+/*
+As you may know, once some people pass their teens, they jokingly only celebrate their 20th or 21st birthday, forever. With some maths skills, that's totally possible - you only need to select the correct number base!
+
+For example, if they turn 32, that's exactly 20 - in base 16... Already 39? That's just 21, in base 19!
+
+Your task is to translate the given age to the much desired 20 (or 21) years, and indicate the number base, in the format specified
+*/
+
+function womensAge(n) {
+  return `${n}? That's just ${20+(n%2)}, in base ${Math.floor(n/2)}!`
+}
