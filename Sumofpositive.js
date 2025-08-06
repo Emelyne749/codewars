a477da12f34f7b57dec280d15c33b30d66d297fd
Emelyne1024 <emelyneishimwe749@gmail.com>
2025-08-06 14:57:27 +0200

    Adding a new kata

---
+++ Sumofpositive.js
@@ -0,0 +1,27 @@
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example
+[1, -4, 7, 12] => 
+1
++
+7
++
+12
+=
+20
+1+7+12=20
+Note
+If there is nothing to sum, the sum is default to 0.
+
+
+*/
+
+
+function positiveSum(arr) {
+  return arr.reduce((sum=0, a)=>{
+    if(a>0) return sum=sum+a
+    else return sum
+  },0)
+  
+}
