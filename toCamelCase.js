b5169e745807840b59ce2e4361774ef76a2d5d63
Emelyne1024 <emelyneishimwe749@gmail.com>
2025-08-29 13:46:39 +0200

    Adding a 6kyu kata

---
+++ toCamelCase.js
@@ -0,0 +1,17 @@
+/*
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+*/
+
+
+function toCamelCase(str){
+  return str.replaceAll("_", ",").replaceAll("-", ",").split(",").map((a, i)=>(i!=0) ? a.replace(a[0],(a[0].toUpperCase())): a ).join("")
+}
+
+console.log(toCamelCase("the_stealth_Warrior"))
